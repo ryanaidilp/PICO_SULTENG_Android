392ece74614da2a25e7e2f2709ba7f1c3ed5b821
RyanPratama <221709679@stis.ac.id>
2020-03-24 15:05:02 +0800

    Revert "Update besar-besaran :"
    This reverts commit d1f53b8dbcb1ea82ae089e954f443178bb1ca226.

--- app/src/main/java/com/banuacoders/covidcheck/adapter/ListMenuAdapter.java
+++ app/src/main/java/com/banuacoders/covidcheck/adapter/ListMenuAdapter.java
@@ -1,22 +1,17 @@
 package com.banuacoders.covidcheck.adapter;
 
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.banuacoders.covidcheck.DetexiActivity;
 import com.banuacoders.covidcheck.R;
-import com.banuacoders.covidcheck.StatsActivity;
 import com.banuacoders.covidcheck.object.MenuItem;
 import com.bumptech.glide.Glide;
-import com.google.android.material.card.MaterialCardView;
 
 import java.util.ArrayList;
 
@@ -37,30 +32,12 @@ public class ListMenuAdapter extends RecyclerView.Adapter<ListMenuAdapter.MenuVi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MenuViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull MenuViewHolder holder, int position) {
         holder.tvDescMenu.setText(listMenu.get(position).getDesc());
         holder.tvTitleMenu.setText(listMenu.get(position).getTitle());
-        holder.cardMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (titleCheck(position, "STATISTIK") || titleCheck(position, "STATISTICS")) {
-                    Intent intent = new Intent(view.getContext(), StatsActivity.class);
-                    view.getContext().startActivity(intent);
-                } else if (titleCheck(position, "RUMAH") || titleCheck(position, "HOSPITAL")) {
-                    Toast.makeText(view.getContext(), listMenu.get(position).getTitle(), Toast.LENGTH_SHORT).show();
-                } else if (titleCheck(position, "SCREENING")) {
-                    Intent intent = new Intent(view.getContext(), DetexiActivity.class);
-                    view.getContext().startActivity(intent);
-                }
-            }
-        });
         Glide.with(holder.itemView)
                 .load(listMenu.get(position).getIcon())
                 .into(holder.iconMenu);
-    }
-
-    private boolean titleCheck(int index, String text) {
-        return listMenu.get(index).getTitle().contains(text);
     }
 
     @Override
@@ -71,14 +48,12 @@ public class ListMenuAdapter extends RecyclerView.Adapter<ListMenuAdapter.MenuVi
     public class MenuViewHolder extends RecyclerView.ViewHolder {
         ImageView iconMenu;
         TextView tvDescMenu, tvTitleMenu;
-        MaterialCardView cardMenu;
 
         public MenuViewHolder(@NonNull View itemView) {
             super(itemView);
             iconMenu = itemView.findViewById(R.id.logo_menu_item);
             tvDescMenu = itemView.findViewById(R.id.desc_menu_item);
             tvTitleMenu = itemView.findViewById(R.id.title_menu_item);
-            cardMenu = itemView.findViewById(R.id.card_menu_item);
         }
     }
 }
--- app/src/main/java/com/banuacoders/covidcheck/network/Api.java
+++ app/src/main/java/com/banuacoders/covidcheck/network/Api.java
@@ -1,25 +1,6 @@
 package com.banuacoders.covidcheck.network;
 
-import java.util.Map;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.QueryMap;
 
 public interface Api {
-
-    @GET("query")
-    Call<ResponseBody> getCovidStats(
-            @QueryMap Map<String, Object> queryMap
-    );
-
-
-    @GET("query")
-    Call<ResponseBody> getCovidProvince(
-            @QueryMap Map<String, Object> queryMap
-    );
-
-    @GET("kabupaten")
-    Call<ResponseBody> getAllKabupaten();
 }
